23907acec2958f33c5172d54d01e6ca3bfc00335
Sayan <sayan.swadesh@gmail.com>
2022-09-14 10:28:59 +0530

    sign up

--- server/controller/AuthController.js
+++ server/controller/AuthController.js
@@ -1,16 +1,28 @@
-import UserModel from '../model/User.js';
+import { UserModel } from '../model/User.js';
+import bcrypt from 'bcryptjs';
+import Jwt from 'jsonwebtoken';
 
-const signup = (req, res) => {
+const signup = async (req, res) => {
   try {
-    // var userDetails = new UserModel({
-    //   name: req.body.name,
-    //   email: req.body.email,
-    // });
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let salt = bcrypt.genSaltSync(10);
+    password = bcrypt.hashSync(password, salt);
+
+    await UserModel.create({
+      name: name,
+      email: email,
+      password: password,
+    });
+
     res.status(200).json({ status: true, msg: 'Signup Successfully' });
   } catch (e) {
     res.status(500).json({ status: false, msg: e });
   }
 };
+
 const signin = (req, res) => {
   try {
     res.status(200).json({ status: true, msg: 'Signin Route' });
--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import 'dotenv/config';
 import routes from './routes/auth.js';
 import dbConnect from './lib/dbConnect.js';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/api', routes);
 dbConnect;
 
